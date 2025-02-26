4f8b89a862d25640b78d61fc94dfb19c6cb303a7
Phil Merrell <philmerrell@boisestate.edu>
2025-02-25 22:05:54 -0700

    feat: Implement create assistant modal with form validation

---
+++ client/src/app/side-nav/assistants-menu/components/create-assistant-modal/assistant-details/assistant-details.component.ts
@@ -0,0 +1,38 @@
+import { Component, ElementRef, inject, Input, OnInit } from '@angular/core';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { IonNav, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonContent, IonItemDivider, IonInput, IonTextarea, ModalController, IonFooter, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronForwardOutline } from 'ionicons/icons';
+import { validateAllFormFields } from 'src/app/shared/form-utils';
+
+@Component({
+  selector: 'app-assistant-details',
+  templateUrl: './assistant-details.component.html',
+  styleUrls: ['./assistant-details.component.scss'],
+  standalone: true,
+  imports: [IonIcon, IonFooter, IonTextarea, IonInput, IonItemDivider, IonContent, IonButton, IonButtons, IonHeader, IonToolbar, IonTitle, ReactiveFormsModule]
+})
+export class AssistantDetailsComponent  implements OnInit {
+  @Input() form!: FormGroup;
+  @Input() nav!: ElementRef<IonNav>;
+  private modalController: ModalController = inject(ModalController);
+
+  constructor() {
+    addIcons({chevronForwardOutline});
+  }
+
+  ngOnInit() {}
+
+  dismiss() {
+    this.modalController.dismiss();
+  }
+
+  next() {
+    if(this.form.controls['name'].valid && this.form.controls['instructions'].valid) {
+      // this.nav.nativeElement.setRoot(GeneralInfoComponent, { nav: this.nav.nativeElement, form: this.form });
+    } else {
+      validateAllFormFields(this.form);
+    }
+  }
+
+}
